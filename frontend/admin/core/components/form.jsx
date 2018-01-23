e3e4ed830bbf1462b9832109a2ba545529ec4ec6
Vadzim Ramanenka <vramanenka@gmail.com>
2018-01-23 23:47:32 +0300

    buttons for form

--- frontend/admin/core/components/form.jsx
+++ frontend/admin/core/components/form.jsx
@@ -2,6 +2,16 @@ import React from 'react'
 
 export default class Form extends React.Component {
     render() {
-        return <div>This is form</div>
+        return (<div>
+            {this.props.children}
+            <div className="field is-grouped">
+                <p class="control">
+                    <button className="button is-link">Save</button>
+                </p>
+                <p class="control">
+                    <button class="button">Cancel</button>
+                </p>
+            </div>
+        </div>)
     }
 }
