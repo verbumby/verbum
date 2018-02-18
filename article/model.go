f132203c0d9a65432dd87175a19386d198a9e9fb
Vadzim Ramanenka <vramanenka@gmail.com>
2018-02-18 12:47:04 +0300

    index article headwords

--- article/model.go
+++ article/model.go
@@ -1,17 +1,7 @@
 package article
 
 import (
-	"fmt"
-	"io"
-	"strconv"
-	"strings"
-
-	"github.com/verbumby/verbum/fts"
-
-	"github.com/verbumby/verbum/headword"
-
 	"github.com/pkg/errors"
-	"golang.org/x/net/html"
 )
 
 //go:generate reform
@@ -36,125 +26,11 @@ func (a *Article) BeforeUpdate() error {
 
 // BeforeSave updates sphinx index/indices
 func (a *Article) BeforeSave() error {
-
-	queue := []html.Token{}
-	z := html.NewTokenizer(strings.NewReader(a.Content))
-	for {
-		tt := z.Next()
-		if tt == html.ErrorToken {
-			err := z.Err()
-			if err != io.EOF {
-				return errors.Wrap(err, "tokenize html")
-			}
-			break
-		}
-
-		t := z.Token()
-		queue = append(queue, t)
-		if tt == html.EndTagToken {
-			startingTagIDX := len(queue) - 1
-			for startingTagIDX >= 0 && queue[startingTagIDX].Type != html.StartTagToken {
-				startingTagIDX--
-			}
-			if startingTagIDX < 0 {
-				return fmt.Errorf("no starting tag found for closing tag %s", t.Type)
-			}
-			if err := processTokenQueue(queue[startingTagIDX:], a); err != nil {
-				return errors.Wrap(err, "processing tags sequence")
-			}
-		}
+	p := parser{
+		a: a,
 	}
-
-	content := ""
-	for _, t := range queue {
-		content += t.String()
+	if err := p.parse(); err != nil {
+		return errors.Wrap(err, "parse article")
 	}
-	a.Content = content
-	return nil
-}
-
-func processTokenQueue(q []html.Token, a *Article) error {
-	if len(q) < 2 {
-		return fmt.Errorf("expected at least two (openning and closing) tokens")
-	}
-
-	qs := q[0]
-	qe := q[len(q)-1]
-
-	if qs.Type != html.StartTagToken {
-		return fmt.Errorf("expected starting token type to be %s, got %s", html.StartTagToken, qs.Type)
-	}
-
-	if q[len(q)-1].Type != html.EndTagToken {
-		return fmt.Errorf("expected ending token type to be %s, got %s", html.EndTagToken, qe.Type)
-	}
-
-	// check whether start and end tokens match
-	if qs.Data != qe.Data {
-		return fmt.Errorf("closing tag %s doesn't match opening tag %s", qe.Data, qs.Data)
-	}
-
-	if qs.Data != "v-hw" {
-		return nil
-	}
-
-	hwcontent := ""
-	for _, t := range q[1 : len(q)-1] {
-		hwcontent += t.String()
-	}
-
-	hw := &headword.Headword{
-		Headword:  hwcontent,
-		ArticleID: a.ID,
-	}
-
-	var idxAttr *html.Attribute
-	for i := range qs.Attr {
-		if qs.Attr[i].Key == "idx" {
-			idxAttr = &qs.Attr[i]
-		}
-	}
-	if idxAttr == nil {
-		qs.Attr = append(qs.Attr, html.Attribute{Key: "idx"})
-		idxAttr = &qs.Attr[len(qs.Attr)-1]
-	}
-
-	tx, err := fts.Sphinx.Begin()
-	if err != nil {
-		return errors.Wrap(err, "begin sphinx tx")
-	}
-
-	if idxAttr.Val != "" {
-		idx64, err := strconv.ParseInt(idxAttr.Val, 10, 32)
-		if err != nil {
-			tx.Rollback()
-			return fmt.Errorf("%s is not valid id of a headword", idxAttr.Val)
-		}
-
-		hw.ID = int32(idx64)
-	} else {
-		if err := tx.QueryRow("SELECT MAX(id) FROM headwords").Scan(&hw.ID); err != nil {
-			tx.Rollback()
-			return errors.Wrap(err, "select max headword id")
-		}
-
-		hw.ID++
-		idxAttr.Val = strconv.FormatInt(int64(hw.ID), 10)
-	}
-
-	columns := hw.Table().Columns()
-	placeholders := fts.Sphinx.Placeholders(1, len(columns))
-	query := "REPLACE INTO headwords (" + strings.Join(columns, ", ") + ") " +
-		"VALUES (" + strings.Join(placeholders, ", ") + ")"
-	if _, err := fts.Sphinx.Exec(query, hw.Values()...); err != nil {
-		tx.Rollback()
-		return errors.Wrapf(err, "replace headword %d", hw.ID)
-	}
-
-	if err := tx.Commit(); err != nil {
-		tx.Rollback()
-		return errors.Wrapf(err, "commit sphinx tx after headowrd %d replace", hw.ID)
-	}
-
 	return nil
 }
---
+++ article/parser.go
@@ -0,0 +1,156 @@
+package article
+
+import (
+	"fmt"
+	"io"
+	"strings"
+
+	"github.com/verbumby/verbum/fts"
+
+	"github.com/verbumby/verbum/headword"
+
+	"github.com/pkg/errors"
+	"golang.org/x/net/html"
+)
+
+type parser struct {
+	a   *Article
+	hws [][]html.Token
+}
+
+func (p *parser) parse() error {
+	a := p.a
+	queue := []html.Token{}
+	z := html.NewTokenizer(strings.NewReader(a.Content))
+	for {
+		tt := z.Next()
+		if tt == html.ErrorToken {
+			err := z.Err()
+			if err != io.EOF {
+				return errors.Wrap(err, "tokenize html")
+			}
+			break
+		}
+
+		t := z.Token()
+		queue = append(queue, t)
+		if tt == html.EndTagToken {
+			startingTagIDX := len(queue) - 1
+			for startingTagIDX >= 0 && queue[startingTagIDX].Type != html.StartTagToken {
+				startingTagIDX--
+			}
+			if startingTagIDX < 0 {
+				return fmt.Errorf("no starting tag found for closing tag %s", t.Type)
+			}
+			if err := p.validateTagTokens(queue[startingTagIDX:]); err != nil {
+				return errors.Wrap(err, "processing tags sequence")
+			}
+
+			p.onTag(queue[startingTagIDX:])
+		}
+	}
+
+	if err := p.indexHeadwords(); err != nil {
+		return errors.Wrap(err, "update headwords")
+	}
+
+	content := ""
+	for _, t := range queue {
+		content += t.String()
+	}
+	a.Content = content
+	return nil
+}
+
+func (p *parser) onTag(tokens []html.Token) {
+	switch tokens[0].Data {
+	case "v-hw":
+		p.onHeadwordTag(tokens)
+	}
+}
+
+func (p *parser) onHeadwordTag(tokens []html.Token) {
+	p.hws = append(p.hws, tokens)
+}
+
+func (p *parser) validateTagTokens(tokens []html.Token) error {
+	if len(tokens) < 2 {
+		return fmt.Errorf("expected at least two (openning and closing) tokens")
+	}
+
+	qs := tokens[0]
+	qe := tokens[len(tokens)-1]
+
+	if qs.Type != html.StartTagToken {
+		return fmt.Errorf("expected starting token type to be %s, got %s", html.StartTagToken, qs.Type)
+	}
+
+	if tokens[len(tokens)-1].Type != html.EndTagToken {
+		return fmt.Errorf("expected ending token type to be %s, got %s", html.EndTagToken, qe.Type)
+	}
+
+	// check whether start and end tokens match
+	if qs.Data != qe.Data {
+		return fmt.Errorf("closing tag %s doesn't match opening tag %s", qe.Data, qs.Data)
+	}
+
+	return nil
+}
+
+func (p *parser) indexHeadwords() error {
+	hws := []headword.Headword{}
+
+	for i, tokens := range p.hws {
+		if i >= 1<<4 {
+			return fmt.Errorf("headword count exceeded allowed %d count", 1<<4)
+		}
+		idx := p.a.ID<<4 + int32(i)
+
+		content := ""
+		for _, t := range tokens[1 : len(tokens)-1] {
+			content += t.String()
+		}
+
+		hws = append(hws, headword.Headword{
+			ID:        idx,
+			Headword:  content,
+			ArticleID: p.a.ID,
+		})
+	}
+
+	tx, err := fts.Sphinx.Begin()
+	if err != nil {
+		return errors.Wrap(err, "begin sphinx tx")
+	}
+
+	ids := make([]interface{}, len(hws))
+
+	for i, hw := range hws {
+		columns := hw.Table().Columns()
+		placeholders := fts.Sphinx.Placeholders(1, len(columns))
+		query := "REPLACE INTO headwords (" + strings.Join(columns, ", ") + ") " +
+			"VALUES (" + strings.Join(placeholders, ", ") + ")"
+		if _, err := fts.Sphinx.Exec(query, hw.Values()...); err != nil {
+			tx.Rollback()
+			return errors.Wrapf(err, "replace headword %d", hw.ID)
+		}
+		ids[i] = hw.ID
+	}
+
+	query := fmt.Sprintf(
+		"DELETE FROM `%s` WHERE id NOT IN (%s)",
+		headword.HeadwordTable.Name(),
+		strings.Join(fts.Sphinx.Placeholders(1, len(ids)), ", "),
+	)
+	if _, err := tx.Exec(query, ids...); err != nil {
+		tx.Rollback()
+		return errors.Wrapf(err, "delete obsolete headwords of article %d", p.a.ID)
+	}
+
+	if err := tx.Commit(); err != nil {
+		tx.Rollback()
+		return errors.Wrap(err, "commit sphinx tx")
+	}
+
+	return nil
+}
