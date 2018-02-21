adc7485e3263b7c632b7cc103a8090cd832b1895
Vadzim Ramanenka <vramanenka@gmail.com>
2018-02-22 01:08:02 +0300

    plain form on dict module

--- frontend/admin/dictionaries/components/form.jsx
+++ frontend/admin/dictionaries/components/form.jsx
@@ -1,11 +1,52 @@
 import React from 'react'
-
-import BaseForm, { InputElement } from '../../core/components/form'
+import { Link } from 'react-router-dom'
 
 export default class Form extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = props.formData
+
+        this.handleInputChange = this.handleInputChange.bind(this)
+        this.handleSubmit = this.handleSubmit.bind(this)
+    }
+
+    handleInputChange(event) {
+        const target = event.target
+        let value = target.type === 'checkbox' ? target.checked : target.value
+        const name = target.name
+
+        this.setState({
+            [name]: value
+        })
+    }
+
+    handleSubmit(event) {
+        event.preventDefault()
+        this.props.onSave({ formData: this.state })
+    }
+
     render() {
-        return (<BaseForm {...this.props}>
-            <InputElement label="Dictionary Name" name="title"/>
-        </BaseForm>)
+        return <form onSubmit={this.handleSubmit}>
+            <div class="field">
+                <label class="label">Title</label>
+                <div class="control">
+                    <input className="input"
+                        type="text"
+                        name="Title"
+                        value={this.state.Title}
+                        onChange={this.handleInputChange}
+                        required
+                    />
+                </div>
+            </div>
+            <div className="field is-grouped">
+                <p class="control">
+                    <button className="button is-link" type="submit">Save</button>
+                </p>
+                <p class="control">
+                    <Link className="button" to={this.props.onCancelRedirectTo}>Cancel</Link>
+                </p>
+            </div>
+        </form>
     }
 }
