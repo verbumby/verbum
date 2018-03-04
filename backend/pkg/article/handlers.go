01c65eabfe03670b8e3bb74d2f1c7a5d3e174bb9
Vadzim Ramanenka <vramanenka@gmail.com>
2018-03-05 01:53:25 +0300

    parse article before save but indes after

--- backend/pkg/article/handlers.go
+++ backend/pkg/article/handlers.go
@@ -2,10 +2,15 @@ package article
 
 import (
 	"encoding/json"
+	"fmt"
 	"net/http"
+	"strings"
 
 	"github.com/pkg/errors"
 	"github.com/verbumby/verbum/backend/pkg/chttp"
+	"github.com/verbumby/verbum/backend/pkg/fts"
+	"github.com/verbumby/verbum/backend/pkg/headword"
+	"github.com/verbumby/verbum/backend/pkg/typeahead"
 	reform "gopkg.in/reform.v1"
 )
 
@@ -21,7 +26,46 @@ func (h *RecordSaveHandler) ServeHTTP(w http.ResponseWriter, ctx *chttp.Context)
 		return nil
 	}
 
-	var err error
+	article := record.(*Article)
+	tokens, err := parseArticle(article.Content)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return nil
+	}
+
+	hws := []reform.Record{}
+	ths := []reform.Record{}
+
+	for i, tokens := range tokens {
+		if i >= 1<<4 {
+			http.Error(w, fmt.Sprintf("headword count exceeded allowed %d count", 1<<4), http.StatusBadRequest)
+			return nil
+		}
+		idx := article.ID<<4 + int32(i)
+
+		content := ""
+		for _, t := range tokens[1 : len(tokens)-1] {
+			content += t.Data
+		}
+
+		hws = append(hws, &headword.Headword{
+			ID:        idx,
+			Headword:  content,
+			ArticleID: article.ID,
+		})
+
+		ths = append(ths, &typeahead.Typeahead{
+			ID:        idx,
+			Typeahead: content,
+			ArticleID: article.ID,
+		})
+	}
+
+	if len(hws) == 0 {
+		http.Error(w, fmt.Sprintf("no headwords, the article won't searchable"), http.StatusBadRequest)
+		return nil
+	}
+
 	if record.HasPK() {
 		err = h.DB.Update(record)
 		if err == reform.ErrNoRows {
@@ -35,20 +79,67 @@ func (h *RecordSaveHandler) ServeHTTP(w http.ResponseWriter, ctx *chttp.Context)
 		return errors.Wrap(err, "save record")
 	}
 
-	if err := index(record); err != nil {
-		return errors.Wrap(err, "save record")
+	if err := indexHeadwords(article.ID, hws); err != nil {
+		return errors.Wrap(err, "index headwords")
+	}
+	if err := indexHeadwords(article.ID, ths); err != nil {
+		return errors.Wrap(err, "index typeaheads")
 	}
 
 	return nil
 }
 
-// index updates sphinx index
-func index(record reform.Struct) error {
-	p := parser{
-		a: record.(*Article),
+func indexHeadwords(articleID int32, records []reform.Record) error {
+	tx, err := fts.Sphinx.Begin()
+	if err != nil {
+		return errors.Wrap(err, "begin sphinx tx")
 	}
-	if err := p.parse(); err != nil {
-		return errors.Wrap(err, "parse article")
+
+	table := records[0].Table()
+	if err := replaceInto(records, tx); err != nil {
+		tx.Rollback()
+		return errors.Wrapf(err, "replace $s of article %d", table.Name(), articleID)
 	}
+
+	firstRecord := records[0].PKValue()
+	lastRecord := records[len(records)-1].PKValue()
+	if err := deleteWhereArticleID(table, articleID, firstRecord, lastRecord, tx); err != nil {
+		tx.Rollback()
+		return errors.Wrapf(err, "delete obsolete headwords of article %d", articleID)
+	}
+
+	if err := tx.Commit(); err != nil {
+		tx.Rollback()
+		return errors.Wrap(err, "commit sphinx tx")
+	}
+
 	return nil
+}
+
+func replaceInto(records []reform.Record, tx reform.TXInterface) error {
+	table := records[0].Table()
+	columns := strings.Join(table.Columns(), ", ")
+
+	placeholders := strings.Join(fts.Sphinx.Placeholders(1, len(table.Columns())), ", ")
+	placeholderRows := []string{}
+	for range records {
+		placeholderRows = append(placeholderRows, "("+placeholders+")")
+	}
+
+	query := "REPLACE INTO " + table.Name() + " (" + columns + ") VALUES " +
+		strings.Join(placeholderRows, ", ")
+
+	values := []interface{}{}
+	for _, record := range records {
+		values = append(values, record.Values()...)
+	}
+
+	_, err := tx.Exec(query, values...)
+	return err
+}
+
+func deleteWhereArticleID(table reform.Table, articleID, idsFirst, idLast interface{}, tx reform.TXInterface) error {
+	query := "DELETE FROM `" + table.Name() + "` WHERE `article_id` = ? AND id NOT BETWEEN ? AND ?"
+	_, err := tx.Exec(query, articleID, idsFirst, idLast)
+	return err
 }
--- backend/pkg/article/parser.go
+++ backend/pkg/article/parser.go
@@ -5,31 +5,20 @@ import (
 	"io"
 	"strings"
 
-	"github.com/verbumby/verbum/backend/pkg/fts"
-	"github.com/verbumby/verbum/backend/pkg/typeahead"
-	reform "gopkg.in/reform.v1"
-
-	"github.com/verbumby/verbum/backend/pkg/headword"
-
 	"github.com/pkg/errors"
 	"golang.org/x/net/html"
 )
 
-type parser struct {
-	a   *Article
-	hws [][]html.Token
-}
-
-func (p *parser) parse() error {
-	a := p.a
+func parseArticle(content string) ([][]html.Token, error) {
+	result := [][]html.Token{}
 	queue := []html.Token{}
-	z := html.NewTokenizer(strings.NewReader(a.Content))
+	z := html.NewTokenizer(strings.NewReader(content))
 	for {
 		tt := z.Next()
 		if tt == html.ErrorToken {
 			err := z.Err()
 			if err != io.EOF {
-				return errors.Wrap(err, "tokenize html")
+				return nil, errors.Wrap(err, "tokenize html")
 			}
 			break
 		}
@@ -42,35 +31,20 @@ func (p *parser) parse() error {
 				startingTagIDX--
 			}
 			if startingTagIDX < 0 {
-				return fmt.Errorf("no starting tag found for closing tag %s", t.Type)
+				return nil, fmt.Errorf("no starting tag found for closing tag %s", t.Type)
 			}
-			if err := p.validateTagTokens(queue[startingTagIDX:]); err != nil {
-				return errors.Wrap(err, "processing tags sequence")
+			if err := validateTagTokens(queue[startingTagIDX:]); err != nil {
+				return nil, errors.Wrap(err, "processing tags sequence")
 			}
 
-			p.onTag(queue[startingTagIDX:])
+			result = append(result, queue[startingTagIDX:])
 		}
 	}
 
-	if err := p.indexHeadwords(); err != nil {
-		return errors.Wrap(err, "update headwords")
-	}
-
-	return nil
+	return result, nil
 }
 
-func (p *parser) onTag(tokens []html.Token) {
-	switch tokens[0].Data {
-	case "v-hw":
-		p.onHeadwordTag(tokens)
-	}
-}
-
-func (p *parser) onHeadwordTag(tokens []html.Token) {
-	p.hws = append(p.hws, tokens)
-}
-
-func (p *parser) validateTagTokens(tokens []html.Token) error {
+func validateTagTokens(tokens []html.Token) error {
 	if len(tokens) < 2 {
 		return fmt.Errorf("expected at least two (openning and closing) tokens")
 	}
@@ -92,92 +66,4 @@ func (p *parser) validateTagTokens(tokens []html.Token) error {
 	}
 
 	return nil
-}
-
-func (p *parser) indexHeadwords() error {
-	hws := []reform.Record{}
-	ths := []reform.Record{}
-
-	for i, tokens := range p.hws {
-		if i >= 1<<4 {
-			return fmt.Errorf("headword count exceeded allowed %d count", 1<<4)
-		}
-		idx := p.a.ID<<4 + int32(i)
-
-		content := ""
-		for _, t := range tokens[1 : len(tokens)-1] {
-			content += t.Data
-		}
-
-		hws = append(hws, &headword.Headword{
-			ID:        idx,
-			Headword:  content,
-			ArticleID: p.a.ID,
-		})
-
-		ths = append(ths, &typeahead.Typeahead{
-			ID:        idx,
-			Typeahead: content,
-			ArticleID: p.a.ID,
-		})
-	}
-
-	if len(hws) == 0 {
-		return nil
-	}
-
-	for _, records := range [][]reform.Record{hws, ths} {
-		tx, err := fts.Sphinx.Begin()
-		if err != nil {
-			return errors.Wrap(err, "begin sphinx tx")
-		}
-
-		table := records[0].Table()
-		if err := replaceInto(records, tx); err != nil {
-			tx.Rollback()
-			return errors.Wrapf(err, "replace $s of article %d", table.Name(), p.a.ID)
-		}
-
-		firstRecord := records[0].PKValue()
-		lastRecord := records[len(hws)-1].PKValue()
-		if err := deleteWhereArticleID(table, p.a.ID, firstRecord, lastRecord, tx); err != nil {
-			tx.Rollback()
-			return errors.Wrapf(err, "delete obsolete headwords of article %d", p.a.ID)
-		}
-
-		if err := tx.Commit(); err != nil {
-			tx.Rollback()
-			return errors.Wrap(err, "commit sphinx tx")
-		}
-	}
-
-	return nil
-}
-
-func replaceInto(records []reform.Record, tx reform.TXInterface) error {
-	table := records[0].Table()
-	columns := strings.Join(table.Columns(), ", ")
-
-	placeholders := strings.Join(fts.Sphinx.Placeholders(1, len(table.Columns())), ", ")
-	placeholderRows := []string{}
-	for range records {
-		placeholderRows = append(placeholderRows, "("+placeholders+")")
-	}
-
-	query := "REPLACE INTO " + table.Name() + " (" + columns + ") VALUES " +
-		strings.Join(placeholderRows, ", ")
-
-	values := []interface{}{}
-	for _, record := range records {
-		values = append(values, record.Values()...)
-	}
-
-	_, err := tx.Exec(query, values...)
-	return err
-}
-
-func deleteWhereArticleID(table reform.Table, articleID, idsFirst, idLast interface{}, tx reform.TXInterface) error {
-	query := "DELETE FROM `" + table.Name() + "` WHERE `article_id` = ? AND id NOT BETWEEN ? AND ?"
-	_, err := tx.Exec(query, articleID, idsFirst, idLast)
-	return err
 }
