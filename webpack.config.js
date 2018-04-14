b6f60c4049b1f62e49d3743f49d05ec8d77445f6
Vadzim Ramanenka <vramanenka@gmail.com>
2018-04-14 20:37:04 +0300

    install dependencies with webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,43 @@
 const path = require('path');
+const webpack = require('webpack');
 
 module.exports = {
-  entry: './frontend/admin/index.jsx',
+  entry: {
+    admin: './frontend/admin/index.jsx',
+  },
   output: {
-    filename: 'admin.js',
+    filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'statics')
+  },
+  optimization: {
+    splitChunks: {
+      cacheGroups: {
+        commons: {
+          test: /[\\/]node_modules[\\/]/,
+          name: "vendor",
+          chunks: "all",
+        },
+      },
+    },
   },
   module: {
     rules: [{
-        test: /\.jsx?$/,
-        loader: 'babel-loader',
+      test: /\.jsx?$/,
+      loader: 'babel-loader',
     }]
   },
   resolve: {
     extensions: ['.js', '.json', '.jsx', '.css']
   },
+  plugins: [
+    new webpack.HashedModuleIdsPlugin(),
+  ],
+  performance: {
+    maxAssetSize: 10 * 1024 * 1024,
+    maxEntrypointSize: 10 * 1024 * 1024,
+  },
   target: 'web',
   mode: 'production',
+  cache: true,
   devtool: 'source-map',
-  externals: {
-    'react': 'React',
-    'react-dom': 'ReactDOM',
-    'redux': 'Redux',
-    'react-redux': 'ReactRedux',
-    'redux-thunk': 'ReduxThunk',
-    'react-router-dom': 'ReactRouterDOM',
-    'simplemde': 'SimpleMDE',
-  }
 };
