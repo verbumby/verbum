69a054fa5916fa440a72692ba5da5667c3efef86
Vadzim Ramanenka <vramanenka@gmail.com>
2018-04-23 01:13:16 +0300

    topnav collapse on sm

--- frontend/admin/core/components/top-nav.jsx
+++ frontend/admin/core/components/top-nav.jsx
@@ -4,7 +4,7 @@ import { Link, Route, withRouter } from 'react-router-dom'
 
 class TopNav extends React.Component {
     render() {
-        return (<nav className="navbar navbar-expand-lg navbar-dark" style={{backgroundColor:'#28a745f0'}}>
+        return (<nav className="navbar navbar-expand-sm navbar-dark" style={{backgroundColor:'#28a745f0'}}>
             <Link className="navbar-brand mb-0 h1" to="/">Verbum</Link>
             <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
