0ba3368730dd611ded4039b335fb954af35abb75
Vadzim Ramanenka <vramanenka@gmail.com>
2019-01-26 23:31:38 +0300

    assemble deb package

---
+++ scripts/deb.bash
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+set -e
+
+root=$(mktemp -d)
+
+mkdir -p $root/usr/local/bin
+cp verbumsrvr verbumctl $root/usr/local/bin
+
+mkdir -p $root/usr/local/share/verbum
+cp -R statics templates $root/usr/local/share/verbum
+
+v="$TRAVIS_BUILD_NUMBER"
+if [ -z "$v" ]; then
+    t=$(date +"%Y%m%d-%H%M%S")
+    h=$(git rev-parse --short HEAD)
+    v="$t-$h"
+fi
+
+mkdir -p $root/DEBIAN
+cat > $root/DEBIAN/control <<EOF
+Package: verbum
+Version: ${v}
+Architecture: amd64
+Essential: no
+Section: web
+Priority: optional
+Depends:
+Maintainer: Vadzim Ramanenka
+Installed-Size:
+Description: Verbum - Online Platform Dictionary.
+EOF
+
+dpkg -b $root verbum_${v}_amd64.deb
+
+rm -r $root
