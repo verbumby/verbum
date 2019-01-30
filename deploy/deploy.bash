370ce6092cc9bfddd9358c0ad185a8197d1c817e
Vadzim Ramanenka <vramanenka@gmail.com>
2019-01-30 17:42:20 +0300

    add the server to the known hosts

--- deploy/deploy.bash
+++ deploy/deploy.bash
@@ -5,6 +5,7 @@ cd deploy
 openssl aes-256-cbc -K $encrypted_861f9db00f37_key -iv $encrypted_861f9db00f37_iv -in id_ed25519.enc -out id_ed25519 -d
 chmod 600 id_ed25519
 sha256sum verbum.deb > verbum.deb.sha256
+ssh-keyscan -H 165.227.129.101 >> ~/.ssh/known_hosts
 sftp -v -oIdentityFile=id_ed25519 deploy@165.227.129.101 <<EOF
 put verbum.deb deps/
 put verbum.deb.sha256 deps/
