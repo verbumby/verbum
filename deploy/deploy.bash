fdd58296b9c90902531e30bf063fd5d583ea2b81
Vadzim Ramanenka <vramanenka@gmail.com>
2019-01-30 22:10:19 +0300

    yet another way to decode id file

--- deploy/deploy.bash
+++ deploy/deploy.bash
@@ -1,8 +1,6 @@
 #!/bin/bash
 set -xe
 cd deploy
-openssl aes-256-cbc -K $encrypted_861f9db00f37_key -iv $encrypted_861f9db00f37_iv -in id_ed25519.enc -out id_ed25519 -d
-chmod 600 id_ed25519
 sha256sum verbum.deb > verbum.deb.sha256
 ssh-keyscan -H 165.227.129.101 >> ~/.ssh/known_hosts
 scp -i id_ed25519 verbum.deb verbum.deb.sha256 deploy@165.227.129.101:~/
