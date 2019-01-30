4735ba08feb306493fcc9c7dd562f7aa43413cda
Vadzim Ramanenka <vramanenka@gmail.com>
2019-01-30 17:26:50 +0300

    Deploy via travis

---
+++ deploy/deploy.bash
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+cd deploy
+
+openssl aes-256-cbc -K $encrypted_861f9db00f37_key -iv $encrypted_861f9db00f37_iv -in id_ed25519.enc -out id_ed25519 -d
+chmod 600 id_ed25519
+sha256sum verbum.deb > verbum.deb.sha256
+sftp -v -oIdentityFile=id_ed25519 deploy@165.227.129.101 <<EOF
+put verbum.deb deps/
+put verbum.deb.sha256 deps/
+EOF
