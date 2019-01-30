ecb43db5cf44e23046c92d7ed52bceef9d7dbf52
Vadzim Ramanenka <vramanenka@gmail.com>
2019-01-30 23:05:59 +0300

    deploy: try to decrypt id key in deploy script again

--- deploy/deploy.bash
+++ deploy/deploy.bash
@@ -1,6 +1,8 @@
 #!/bin/bash
-set -xe
+set -e
 cd deploy
+openssl aes-256-cbc -K $encrypted_a25db77e2a26_key -iv $encrypted_a25db77e2a26_iv -in id_ed25519.enc -out id_ed25519 -d
+chmod 600 id_ed25519
 sha256sum verbum.deb > verbum.deb.sha256
 ssh-keyscan -H 165.227.129.101 >> ~/.ssh/known_hosts
 scp -i id_ed25519 verbum.deb verbum.deb.sha256 deploy@165.227.129.101:~/
