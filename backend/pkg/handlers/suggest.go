3ef1f0a80679900c8f1b5227f1860dcd0d74158a
Vadzim Ramanenka <vramanenka@gmail.com>
2019-02-06 21:21:49 +0300

    use helper functions to query elastic

--- backend/pkg/handlers/suggest.go
+++ backend/pkg/handlers/suggest.go
@@ -2,56 +2,32 @@ package handlers
 
 import (
 	"encoding/json"
-	"fmt"
-	"io/ioutil"
 	"net/http"
-	"strings"
 
 	"github.com/pkg/errors"
-	"github.com/spf13/viper"
 	"github.com/verbumby/verbum/backend/pkg/chttp"
+	"github.com/verbumby/verbum/backend/pkg/storage"
 )
 
 // Suggest handles _suggest http request
 func Suggest(w http.ResponseWriter, rctx *chttp.Context) error {
 	q := rctx.R.URL.Query().Get("q")
 
-	qbytes, err := json.Marshal(q)
-	if err != nil {
-		return errors.Wrap(err, "marshal q")
-	}
-	query := `{
+	reqbody := map[string]interface{}{
 		"_source": false,
-		"suggest": {
-			"HeadwordSuggest": {
-				"prefix": ` + string(qbytes) + `,
-				"completion": {
-					"field": "Suggest",
+		"suggest": map[string]interface{}{
+			"HeadwordSuggest": map[string]interface{}{
+				"prefix": q,
+				"completion": map[string]interface{}{
+					"field":           "Suggest",
 					"skip_duplicates": true,
-					"size": 10
-				}
-			}
-		}
-	}`
-
-	url := viper.GetString("elastic.addr") + "/dict-*/_search"
-	resp, err := http.Post(url, "application/json", strings.NewReader(query))
-	if err != nil {
-		return errors.Wrap(err, "query elastic")
-	}
-	defer resp.Body.Close()
-
-	if resp.StatusCode != http.StatusOK {
-		respbytes, _ := ioutil.ReadAll(resp.Body)
-		return fmt.Errorf(
-			"query elastic: expected %d, got %d: %s",
-			http.StatusOK,
-			resp.StatusCode,
-			string(respbytes),
-		)
+					"size":            10,
+				},
+			},
+		},
 	}
 
-	respdata := struct {
+	respbody := struct {
 		Suggest struct {
 			HeadwordSuggest []struct {
 				Options []struct {
@@ -60,12 +36,13 @@ func Suggest(w http.ResponseWriter, rctx *chttp.Context) error {
 			}
 		} `json:"suggest"`
 	}{}
-	if err := json.NewDecoder(resp.Body).Decode(&respdata); err != nil {
-		return errors.Wrap(err, "unmarshal elastic resp")
+
+	if err := storage.Post("/dict-*/_search", reqbody, &respbody); err != nil {
+		return errors.Wrap(err, "query elastic")
 	}
 
 	data := []string{}
-	for _, hws := range respdata.Suggest.HeadwordSuggest {
+	for _, hws := range respbody.Suggest.HeadwordSuggest {
 		for _, opt := range hws.Options {
 			data = append(data, opt.Text)
 		}
