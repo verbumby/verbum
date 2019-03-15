94a160cde1ced8cd011f6c56a4edfb665781141e
Vadzim Ramanenka <vramanenka@gmail.com>
2019-03-15 11:49:58 +0300

    slugify function

--- backend/pkg/textutil/romanize.go
+++ backend/pkg/textutil/romanize.go
@@ -1,6 +1,9 @@
 package textutil
 
-import "strings"
+import (
+	"regexp"
+	"strings"
+)
 
 var belarusianConsonats = []rune{
 	'Б', 'В', 'Г', 'Д', 'Ж', 'З', 'Й', 'К', 'Л', 'М', 'Н',
@@ -121,4 +124,17 @@ func RomanizeBelarusian(s string) string {
 		prev = r
 	}
 	return sb.String()
+}
+
+// Slugify creates a url slug from the s string
+func Slugify(s string) string {
+	s = strings.ToLower(s)
+	r := regexp.MustCompile("[^a-z0-9-]")
+	s = r.ReplaceAllString(s, "-")
+
+	for strings.Contains(s, "--") {
+		s = strings.Replace(s, "--", "-", -1)
+	}
+
+	return s
 }
