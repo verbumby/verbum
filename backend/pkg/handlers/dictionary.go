55b20552d2652e81935b8d146a667aff359e3af8
Vadzim Ramanenka <vramanenka@gmail.com>
2019-04-13 13:28:55 +0300

    letter filter on dictionary page

--- backend/pkg/handlers/dictionary.go
+++ backend/pkg/handlers/dictionary.go
@@ -1,6 +1,7 @@
 package handlers
 
 import (
+	"fmt"
 	"math"
 	"net/http"
 
@@ -10,6 +11,7 @@ import (
 	"github.com/verbumby/verbum/backend/pkg/chttp"
 	"github.com/verbumby/verbum/backend/pkg/dictionary"
 	"github.com/verbumby/verbum/backend/pkg/htmlui"
+	"github.com/verbumby/verbum/backend/pkg/storage"
 	"github.com/verbumby/verbum/backend/pkg/tm"
 )
 
@@ -25,15 +27,114 @@ func Dictionary(w http.ResponseWriter, rctx *chttp.Context) error {
 
 	urlQuery := htmlui.Query([]htmlui.QueryParam{
 		htmlui.NewIntegerQueryParam("page", 1),
+		htmlui.NewStringQueryParam("prefix", ""),
 	})
 	urlQuery.From(rctx.R.URL.Query())
 
+	min := func(a, b int) int {
+		if a < b {
+			return a
+		}
+		return b
+	}
+
+	prefix := []rune(urlQuery.Get("prefix").(*htmlui.StringQueryParam).Value())
+	prefix = prefix[:min(3, len(prefix))]
+	allaggs := map[string]interface{}{}
+	for i := 0; i < min(len(prefix), 2)+1; i++ {
+		aggs := map[string]interface{}{
+			fmt.Sprintf("Letter%d", i+1): map[string]interface{}{
+				"terms": map[string]interface{}{
+					"field": fmt.Sprintf("Prefix.Letter%d", i+1),
+					"size":  200,
+					"order": map[string]interface{}{"_key": "asc"},
+				},
+			},
+		}
+		for j := 0; j < i; j++ {
+			aggs = map[string]interface{}{
+				fmt.Sprintf("Letter%d", i+1): map[string]interface{}{
+					"filter": map[string]interface{}{
+						"term": map[string]interface{}{
+							fmt.Sprintf("Prefix.Letter%d", j+1): string(prefix[j]),
+						},
+					},
+					"aggs": aggs,
+				},
+			}
+		}
+
+		for k, v := range aggs {
+			allaggs[k] = v
+		}
+	}
+
+	aggsreqbody := map[string]interface{}{
+		"size": 0,
+		"aggs": map[string]interface{}{
+			"Prefix": map[string]interface{}{
+				"nested": map[string]interface{}{"path": "Prefix"},
+				"aggs":   allaggs,
+			},
+		},
+	}
+
+	type aggresult struct {
+		Buckets []htmlui.LetterFilterEntity `json:"buckets"`
+	}
+	aggsrespbody := struct {
+		Aggregations struct {
+			Prefix struct {
+				Letter1 *aggresult
+				Letter2 *struct{ Letter2 aggresult }
+				Letter3 *struct{ Letter3 struct{ Letter3 aggresult } }
+			}
+		} `json:"aggregations"`
+	}{}
+	if err := storage.Post("/dict-"+dictID+"/_search", aggsreqbody, &aggsrespbody); err != nil {
+		return errors.Wrap(err, "aggs query")
+	}
+	letterFilter := htmlui.LetterFilter{
+		Prefix: prefix,
+		LetterLink: func(prefix string) string {
+			urlQuery := urlQuery.Clone()
+			urlQuery.Get("page").Reset()
+			urlQuery.Get("prefix").(*htmlui.StringQueryParam).SetValue(prefix)
+			return "?" + urlQuery.Encode()
+		},
+	}
+	letterFilter.AddLevel(aggsrespbody.Aggregations.Prefix.Letter1.Buckets)
+	if aggsrespbody.Aggregations.Prefix.Letter2 != nil {
+		letterFilter.AddLevel(aggsrespbody.Aggregations.Prefix.Letter2.Letter2.Buckets)
+	}
+	if aggsrespbody.Aggregations.Prefix.Letter3 != nil {
+		letterFilter.AddLevel(aggsrespbody.Aggregations.Prefix.Letter3.Letter3.Letter3.Buckets)
+	}
+
 	const pageSize = 10
+	prefixMusts := []interface{}{}
+	for i, p := range prefix {
+		prefixMusts = append(prefixMusts, map[string]interface{}{
+			"term": map[string]interface{}{
+				fmt.Sprintf("Prefix.Letter%d", i+1): string(p),
+			},
+		})
+	}
 	articles, total, err := article.Query("/dict-"+dictID+"/_search", map[string]interface{}{
 		"from": (urlQuery.Get("page").(*htmlui.IntegerQueryParam).Value() - 1) * pageSize,
 		"size": pageSize,
 		"sort": []interface{}{
 			"Title",
+		},
+		"query": map[string]interface{}{
+			"nested": map[string]interface{}{
+				"path": "Prefix",
+				"query": map[string]interface{}{
+					"bool": map[string]interface{}{
+						"must": prefixMusts,
+					},
+				},
+			},
 		},
 	})
 	if err != nil {
@@ -46,11 +147,13 @@ func Dictionary(w http.ResponseWriter, rctx *chttp.Context) error {
 		Dictionary      dictionary.Dictionary
 		Articles        []article.Article
 		Pagination      htmlui.Pagination
+		LetterFilter    htmlui.LetterFilter
 	}{
 		PageTitle:       dict.Title,
 		PageDescription: dict.Title,
 		Dictionary:      dict,
 		Articles:        articles,
+		LetterFilter:    letterFilter,
 		Pagination: htmlui.Pagination{
 			Current: urlQuery.Get("page").(*htmlui.IntegerQueryParam).Value(),
 			Total:   int(math.Ceil(float64(total) / pageSize)),
