5f57951cb117dad0db9e5e3cf1215a183854118e
Vadzim Ramanenka <vramanenka@gmail.com>
2020-04-26 17:57:54 +0200

    Deploy to hetzner

--- deploy/deploy.bash
+++ deploy/deploy.bash
@@ -4,8 +4,8 @@ cd deploy
 openssl aes-256-cbc -K $encrypted_a25db77e2a26_key -iv $encrypted_a25db77e2a26_iv -in id_ed25519.enc -out id_ed25519 -d
 chmod 600 id_ed25519
 sha256sum verbum.deb > verbum.deb.sha256
-ssh-keyscan -H 165.227.129.101 >> ~/.ssh/known_hosts
-sftp -i id_ed25519 deploy@165.227.129.101 <<EOF
+ssh-keyscan -H 49.12.79.27 >> ~/.ssh/known_hosts
+sftp -i id_ed25519 deploy@49.12.79.27 <<EOF
 cd debdelivery
 put verbum.deb
 put verbum.deb.sha256
