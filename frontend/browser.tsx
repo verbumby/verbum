e82f76c44679c69d5d8addfbe95988d57334b671
Vadzim Ramanenka <vramanenka@gmail.com>
2020-11-07 23:26:48 +0300

    fe: browser react hydrate

--- frontend/browser.tsx
+++ frontend/browser.tsx
@@ -1,1 +1,6 @@
-console.log("hello!")
+import * as React from 'react'
+import { hydrate } from 'react-dom'
+
+import { App } from './app/App'
+
+hydrate(<App message="ololo" />, document.querySelector('body .content'))
