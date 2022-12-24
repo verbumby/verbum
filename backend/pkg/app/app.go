9bdf85b603109c67648b393eef47056afcc7dd35
Vadzim Ramanenka <vramanenka@gmail.com>
2022-12-24 19:58:07 +0100

    remove article migrations

--- backend/pkg/app/app.go
+++ backend/pkg/app/app.go
@@ -4,9 +4,6 @@ import (
 	"fmt"
 
 	"github.com/spf13/viper"
-	"github.com/verbumby/verbum/backend/pkg/article"
-	"github.com/verbumby/verbum/backend/pkg/dictionary"
-	"github.com/verbumby/verbum/backend/pkg/storage"
 )
 
 // Bootstrap bootstraps the application
@@ -36,93 +33,6 @@ func Bootstrap() error {
 
 	if err := viper.ReadInConfig(); err != nil {
 		return fmt.Errorf("read in config: %w", err)
-	}
-
-	if err := article.Migrate(); err != nil {
-		return fmt.Errorf("article migrate: %w", err)
-	}
-
-	return nil
-
-	/*
-		if err := elasticIndexTemplatesMigrate(); err != nil {
-			return fmt.Errorf("elastic index templates migrate: %w", err)
-		}
-
-		if err := timestampFieldMigrate(); err != nil {
-			return fmt.Errorf("timestamp field migration: %w", err)
-		}
-
-		return nil
-	*/
-}
-
-func elasticIndexTemplatesMigrate() error {
-	respbody := &struct {
-		AccessLog struct {
-			Version int
-		} `json:"access-log"`
-	}{}
-	err := storage.Get("/_template/access-log?filter_path=*.version", respbody)
-
-	if err != nil || respbody.AccessLog.Version != 1 {
-		err = storage.Put("/_template/access-log", map[string]interface{}{
-			"version":        1,
-			"index_patterns": []string{"access-log-*"},
-			"settings": map[string]interface{}{
-				"number_of_shards":   1,
-				"number_of_replicas": 0,
-			},
-			"mappings": map[string]interface{}{
-				"_doc": map[string]interface{}{
-					"properties": map[string]interface{}{
-						"TS":        map[string]interface{}{"type": "date"},
-						"Path":      map[string]interface{}{"type": "keyword"},
-						"Query":     map[string]interface{}{"type": "keyword"},
-						"IP":        map[string]interface{}{"type": "keyword"},
-						"UserAgent": map[string]interface{}{"type": "keyword"},
-						"Referer":   map[string]interface{}{"type": "keyword"},
-					},
-				},
-			},
-		}, nil)
-		if err != nil {
-			return fmt.Errorf("create access log index template: %w", err)
-		}
-	}
-
-	return nil
-}
-
-func timestampFieldMigrate() error {
-	dicts := dictionary.GetAll()
-
-	for _, d := range dicts {
-		respbody := map[string]struct {
-			Mappings map[string]map[string]interface{} `json:"mappings"`
-		}{}
-		if err := storage.Get("/dict-"+d.ID()+"/_mapping", &respbody); err != nil {
-			return fmt.Errorf("get mappings: %w", err)
-		}
-
-		if _, ok := respbody["dict-"+d.ID()].Mappings["ModifiedAt"]; ok {
-			return nil
-		}
-
-		err := storage.Put(
-			"/dict-"+d.ID()+"/_mapping",
-			map[string]interface{}{
-				"properties": map[string]interface{}{
-					"ModifiedAt": map[string]interface{}{
-						"type": "date",
-					},
-				},
-			},
-			nil,
-		)
-		if err != nil {
-			return fmt.Errorf("migrate %s dict: %w", d.ID(), err)
-		}
 	}
 
 	return nil
