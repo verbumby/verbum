92eab4f1cf7c89acc2555119d67364d8851fb269
Vadzim Ramanenka <vramanenka@gmail.com>
2023-05-12 12:39:46 +0200

    Add daviedka link in footer

--- frontend/common/Footer.tsx
+++ frontend/common/Footer.tsx
@@ -2,8 +2,11 @@ import * as React from 'react'
 
 export const Footer: React.VoidFunctionComponent = () => (
     <footer className="text-center" style={{ marginTop: 'auto' }}>
-        <a className="btn btn-link text-secondary" href="mailto:vramanenka@gmail.com">
+        <a className="btn btn-link btn-sm text-secondary" href="mailto:vramanenka@gmail.com">
             vramanenka@gmail.com
+        </a>
+        <a className="btn btn-link btn-sm text-secondary" target="_blank" href="https://daviedka.bnkorpus.info">
+            Моўная даведка Інстытута мовазнаўства
         </a>
     </footer>
 )
