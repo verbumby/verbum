68940d14d7b823afaad565c9b2866b1b48df70cd
Vadzim Ramanenka <vramanenka@gmail.com>
2023-05-25 21:36:59 +0200

    Switch to esbm-5

--- backend/pkg/dictionary/dictionary.go
+++ backend/pkg/dictionary/dictionary.go
@@ -34,7 +34,7 @@ var dictionaries = []Dictionary{
 	},
 	DSL{
 		id:      "esbm",
-		indexID: "esbm-4",
+		indexID: "esbm-5",
 		title:   "Этымалагічны слоўнік беларускай мовы (1978-2017)",
 		abbrevs: parseDSLAbbrev(esbmAbbrev),
 	},
