9f0111d4eebee77014c07bf5bf07f538ec9fea65
Vadzim Ramanenka <vramanenka@gmail.com>
2024-02-22 20:29:46 +0100

    Add GitHub link

--- frontend/common/Footer.tsx
+++ frontend/common/Footer.tsx
@@ -1,7 +1,12 @@
 import * as React from 'react'
+import { IconGitHub } from '../icons'
 
 export const Footer: React.VoidFunctionComponent = () => (
     <footer className="text-center" style={{ marginTop: 'auto' }}>
+        <a className="btn btn-link btn-sm text-secondary" target="_blank" href="https://github.com/verbumby/verbum">
+            <IconGitHub />
+        </a>
+        {' '}
         <a className="btn btn-link btn-sm text-secondary" href="mailto:vramanenka@gmail.com">
             vramanenka@gmail.com
         </a>
